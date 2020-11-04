a062435c381d4596130b0e2d199ac7abcd8d1c77
timmmmmb <frey.tim@outlook.com>
2020-11-05 00:49:35 +0100

    added a simple animation demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,120 @@
+// #docregion ShakeCurve
+import 'dart:math';
+
+// #enddocregion ShakeCurve
+import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MyWidget(title: 'Demo App', message: 'Hello'));
+void main() => runApp(LogoApp());
 
-class MyWidget extends StatelessWidget {
-  final String title;
-  final String message;
+// #docregion diff
+class AnimatedLogo extends AnimatedWidget {
+  // Make the Tweens static because they don't change.
+  static final _opacityTween = Tween<double>(begin: 0, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 125);
+  static final _colorTween = ColorTween(begin: Color.fromRGBO(255, 0, 0, 1), end: Color.fromRGBO(0, 255, 0, 1));
 
-  const MyWidget({
-    Key key,
-    @required this.title,
-    @required this.message,
-  }) : super(key: key);
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
 
-  @override
   Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Animation demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text(title),
+          title: Text('Flutter Animation demo'),
         ),
-        body: Center(
-          child: Text(message, style: GoogleFonts.pacifico()),
+        body: SizedBox(
+        child: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Text('Simple Size Animation', style: TextStyle(fontWeight: FontWeight.w500))
+            ),
+            Center(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical:(125-_sizeTween.evaluate(animation))/2,),
+                  height: _sizeTween.evaluate(animation),
+                  width: _sizeTween.evaluate(animation),
+                  child: FlutterLogo(),
+                  ),
+                ),
+            Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Text('Simple Fading Animation', style: TextStyle(fontWeight: FontWeight.w500))
+            ),
+            Center(
+              child: Opacity(
+                opacity: _opacityTween.evaluate(animation),
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: 10),
+                  height: 125,
+                  width: 125,
+                  child: FlutterLogo(),
+                ),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                child: Text('Simple Color Animation', style: TextStyle(fontWeight: FontWeight.w500))
+            ),
+            Center(
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                height: 125,
+                width: 125,
+                color: _colorTween.evaluate(animation),
+              ),
+            ),
+              ]
+          ),
         ),
       ),
     );
   }
+}
+
+class LogoApp extends StatefulWidget {
+  _LogoAppState createState() {
+    return _LogoAppState();
+  }
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // #docregion AnimationController, tweens
+    controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    // #enddocregion AnimationController, tweens
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+// #enddocregion diff
+
+// #docregion ShakeCurve
+class ShakeCurve extends Curve {
+  @override
+  double transform(double t) => sin(t * pi * 2);
 }
